b4b2ef8f7a5d2b46c0e9e7a76c0dafa0454f22a9
kirandevraj <thekirandevraj@gmail.com>
2026-06-08 23:03:18 +0530

    Optimize LeetCode 334 to O(n) two-variable scan

--- 01_array_string/08_increasing_triplet_subsequence.py
+++ 01_array_string/08_increasing_triplet_subsequence.py
@@ -34,11 +34,21 @@ from typing import List
 
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
-        for i in range(len(nums)-2):
-            for j in range(i+1, len(nums)-1):
-                for k in range(j+1, len(nums)):
-                    if nums[i] < nums[j] < nums[k]:
-                        return True
+        # for i in range(len(nums)-2):
+        #     for j in range(i+1, len(nums)-1):
+        #         for k in range(j+1, len(nums)):
+        #             if nums[i] < nums[j] < nums[k]:
+        #                 return True
+        # return False
+
+        first = second = float("inf")
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
         return False
 
 
