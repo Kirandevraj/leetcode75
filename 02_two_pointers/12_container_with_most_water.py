4e4a9098157cc75a7f55636fcf9c31b6cc54ad55
kirandevraj <thekirandevraj@gmail.com>
2026-06-09 23:42:22 +0530

    Add LeetCode 11 solution

--- 02_two_pointers/12_container_with_most_water.py
+++ 02_two_pointers/12_container_with_most_water.py
@@ -28,7 +28,19 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
+        left = 0
+        right = len(height) - 1
+        maxarea = 0
+        while (left != right):
+            area = ((right - left) * min(height[left], height[right]))
+            if area > maxarea:
+                maxarea = area
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxarea
+
 
 
 if __name__ == "__main__":
