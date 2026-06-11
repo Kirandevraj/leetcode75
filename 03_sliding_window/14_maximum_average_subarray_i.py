977a830362bcbbe28cb776a51bc7eb574f60e9e0
kirandevraj <thekirandevraj@gmail.com>
2026-06-11 23:39:39 +0530

    Add LeetCode 643 solution

--- 03_sliding_window/14_maximum_average_subarray_i.py
+++ 03_sliding_window/14_maximum_average_subarray_i.py
@@ -28,7 +28,13 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        pass
+        window_sum = sum(nums[:k])
+        best = window_sum
+        for i in range(1, len(nums)-k+1):
+            window_sum -= nums[i-1]
+            window_sum += nums[i+k-1]
+            best = max(window_sum, best)
+        return best/k
 
 
 if __name__ == "__main__":
